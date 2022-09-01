95224c43f201a1f61d596838e0132cb5b33f1e51
Bambo0k <95869835+Bambo0k@users.noreply.github.com>
2022-09-01 21:08:01 +0300

    Add Card

---
+++ src/store.js
@@ -0,0 +1,19 @@
+import { legacy_createStore as createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import rootReducer from './reducer';
+import logger from 'redux-logger';
+
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () => {
+    let store = createStore(persistedReducer, applyMiddleware(logger));
+    let persistor = persistStore(store);
+    return { store, persistor };
+};
